b1c92472602d9ef8a6e73f1897edb5d3c63ecce7
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2022-12-30 12:20:57 +0100

    Seguimos con otra prueba

--- model.py
+++ model.py
@@ -198,7 +198,7 @@ class MyGCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = GCNConv(2, 64)
-        self.conv2 = GCNConv(64, 2)
+        self.conv2 = GCNConv(64, 1)
 
     def forward(self, x, edge_index):
         x, edge_index = x, edge_index
