c50611f1fffb000678570a4a04eac6126bee2ea2
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2022-12-30 12:18:06 +0100

    Voy a probar otra cosa

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from torch_geometric.nn import GCNConv, Sequential
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
-from model import train_node_classifier, print_results
+from model import train_node_classifier, print_results, MyGCN
 
 
 data_folder_name = "training"
@@ -37,17 +37,15 @@ print("--------------------------------------------")
 # print(in_data.shape)
 print("--------------------------------------------")
 
-print("Empezamos a entrenar")
-node_mlp_model, node_mlp_result = train_node_classifier(model_name="MLP",
-                                                        dataset=graphs,
-                                                        CHECKPOINT_PATH=CHECKPOINT_PATH,
-                                                        c_hidden=16,
-                                                        num_layers=2,
-                                                        dp_rate=0.1)
+# print("Empezamos a entrenar")
+# node_mlp_model, node_mlp_result = train_node_classifier(model_name="MLP",
+#                                                         dataset=graphs,
+#                                                         CHECKPOINT_PATH=CHECKPOINT_PATH,
+#                                                         c_hidden=16,
+#                                                         num_layers=2,
+#                                                         dp_rate=0.1)
 
-print_results(node_mlp_result)
-
-
+# print_results(node_mlp_result)
 
 
 
@@ -56,17 +54,19 @@ print_results(node_mlp_result)
 
 
 
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# model = MyGCN().to(device)
 
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-# #train
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = MyGCN().to(device)
 
-# model.train()
-# for epoch in range(20):
-#     optimizer.zero_grad()
-#     out = model(x = metricas_entrada[0], edge_index=edge_tensor[0])
-#     loss = F.nll_loss(out, metricas_salida[0])
-#     loss.backward()
-#     optimizer.step()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+#train
+
+model.train()
+for epoch in range(20):
+    optimizer.zero_grad()
+    out = model(x = metricas_entrada[0], edge_index=edge_tensor[0])
+    loss = F.nll_loss(out, metricas_salida[0])
+    loss.backward()
+    optimizer.step()
