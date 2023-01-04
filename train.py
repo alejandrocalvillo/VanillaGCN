16b02721aceabb4a3bc85f6fbf769870cb5590e8
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-01-04 13:28:30 +0000

    Final Model

--- train.py
+++ train.py
@@ -32,30 +32,35 @@ comparador = metricas_salida[0:4]
 
 epoch = 200
 
+for i in range(epoch):
+    
+    print("Epoch: ", i)
 
-a = edge_index[0].todense()
-edge_tensor = torch.tensor(a, dtype = torch.long)
-input_edge_tensor = edge_tensor.nonzero().t().contiguous()
+    #Take Adjacency_Matrix and Reshape it in order to fulfill specified shape
+    j = 0 + i
+    if i >= 19:
+        j = 0
+    else:
+        a = edge_index[j].todense()
+        edge_tensor = torch.tensor(a, dtype = torch.long)
+        input_edge_tensor = edge_tensor.nonzero().t().contiguous()
 
 
-testloader = torch.utils.data.DataLoader(metricas_entrada, batch_size=4, shuffle=True)
+    testloader = torch.utils.data.DataLoader(metricas_entrada, batch_size=4, shuffle=True)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = MyGCN().to(device)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = MyGCN().to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-#train
-print(comparador)
-model.train()
-for data in testloader:
-    optimizer.zero_grad()
-    print(f'iteracion, data.size={data.size()}')
-    out = model(x = data , edge_index=input_edge_tensor)
-    print(out)
-    print(out.size())
-    print(comparador[:,0,:])
-    print(comparador[:,0,:].size())
-    loss = F.mse_loss(out, comparador)
-    loss.backward()
-    optimizer.step()
+    #train
+    print(comparador)
+    model.train()
+    for data in testloader:
+        optimizer.zero_grad()
+        print(f'iteracion, data.size={data.size()}')
+        out = model(x = data , edge_index=input_edge_tensor)
+        loss = F.mse_loss(out, comparador)
+        loss.backward()
+        print("Loss: ", loss)
+        optimizer.step()
