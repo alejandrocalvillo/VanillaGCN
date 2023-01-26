574dd3b789bdf954851cb7ee81738583f3e13f33
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-01-26 14:18:47 +0100

    More epoch

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ dataset = torch.utils.data.TensorDataset(data.x, data.y)
 
 
 #Select number of epoch
-epoch = 1000
+epoch = 100000
 
 
 # JORGE: fíjate en un solo caso, es decir, no vayas cambiando de grafos
