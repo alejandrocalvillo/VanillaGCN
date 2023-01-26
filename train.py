7359c45d6fa39199c41d9ca6e6e705371e4419b0
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-01-26 12:50:16 +0100

    Train with labels

--- train.py
+++ train.py
@@ -24,12 +24,11 @@ metricas_entrada, metricas_salida,edge_index = preparation_dataset(src_path)
 
 #Reshape data in order to fulfill specified shape
 metricas_entrada = np.reshape(metricas_entrada, (20, 9, 2))
-metricas_salida = np.reshape(metricas_salida, (20, 9, 1))
+metricas_salida = np.reshape(metricas_salida[:,:,2], (20, 9, 1))
 #comparador = metricas_salida[0:4]
 
-metricas_entrada.append(metricas_salida)
 
-print(metricas_entrada)
+print(metricas_entrada.shape)
 
 # Normaliza datos de entrada y de salida
 # https://pytorch.org/docs/stable/generated/torch.nn.functional.normalize.html#torch-nn-functional-normalize
