90879059cf39f3258a8c2009df92afaecec82d8e
alejandrocalvillo.it <alejandrocalvillo.it@gmail.com>
2023-02-10 10:40:34 +0000

    Evaluation

--- Test_of_model/test.py
+++ Test_of_model/test.py
@@ -70,7 +70,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #The goal is to demostrate that the model is learning, to do so let's take an array of epochs(epochs) and learning rates (lrs)
 model = MyGCN().to(device)
 model.load_state_dict(torch.load("weigths/model_weights5000.0001.pt"))
-model.eval(True)
+model.eval()
 for epoch in epochs:
     for lr in lrs:
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
