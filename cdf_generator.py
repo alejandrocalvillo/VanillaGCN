779a3e508df215a1f691217384f3fd52c44159dc
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-02-14 12:17:11 +0100

    CDF labels

--- cdf_generator.py
+++ cdf_generator.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 def cdf_plot(tensor, name):
     
     sorted_tensor, indices = torch.sort(tensor)
-    y = torch.arange(1, len(tensor) + 1) / len(tensor)
+    x = torch.arange(1, len(tensor) + 1) / len(tensor)
 
     fig, ax = plt.subplots()
-    plt.plot(y,sorted_tensor)
+
+    plt.plot(x,sorted_tensor)
+    plt.title(name+" CDF")
+    if name == "Delay":
+        plt.xlabel("Delay [s]")
     plt.savefig('cdf_plots/' +name+ '.png')
