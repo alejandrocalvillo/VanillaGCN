2d19f08692044a866427dc5bf1a67765671370df
alejandrocalvillo.it <alejandrocalvillo.it@gmail.com>
2023-02-15 10:21:00 +0000

    New CDF

--- cdf_generator.py
+++ cdf_generator.py
@@ -1,5 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
 
 def cdf_plot(tensor, name):
     
@@ -21,7 +22,10 @@ def cdf_plot(tensor, name):
 
 def cdf_hist (tensor):
     fig, ax = plt.subplots()
-    plt.hist(tensor, cumulative=True, label='CDF',
-         histtype='step', alpha=0.8)
+    sorted_tensor, indices = torch.sort(tensor)
+    np.cumsum(sorted_tensor)
+    x = torch.arange(1, len(tensor) + 1) / len(tensor)
+    plt.plot(x,sorted_tensor)
+   # plt.hist(tensor, cumulative=True, label='CDF', histtype='step', alpha=0.8)
     
     plt.savefig('cdf_plots/Delay_HIST_CDF.png')
