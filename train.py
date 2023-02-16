b879508d16bf8f6b39e90923970dcce2560bc825
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-02-16 12:32:30 +0100

    Code cleanning

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 #Core import
 from preparacion_dataset import preparation_dataset, prepare_data, plot_mse_epoch
 
-from cdf_generator import cdf_plot, cdf_hist
 #GCN Model
 from model import MyGCN
 
@@ -23,75 +22,70 @@ src_path = f"{data_folder_name}/results/dataset1/"
 #Load the data
 metricas_entrada,edge_index = preparation_dataset(src_path)
 
-delay  = metricas_entrada[:,2,:]
-delay = np.reshape(delay, (20, 9, 1))
-#cdf_plot(delay, "Delay")
-cdf_hist(delay)
-
 # Normalize data
 # https://pytorch.org/docs/stable/generated/torch.nn.functional.normalize.html#torch-nn-functional-normalize
 
-# metricas_entrada = F.normalize(metricas_entrada)
+metricas_entrada = F.normalize(metricas_entrada)
 
-# #Select features to predict
-# input = metricas_entrada[:,:2,:] #train
-# labels =metricas_entrada[:,2,:] #train
+#Select features to predict
+input = metricas_entrada[:,:2,:] #train
+labels =metricas_entrada[:,2,:] #train
 
-# #Reshape data in order to fulfill specified shape
-# input = np.reshape(input, (20, 9, 2))
-# labels = np.reshape(labels, (20, 9, 1))
+#Reshape data in order to fulfill specified shape
+input = np.reshape(input, (20, 9, 2))
+labels = np.reshape(labels, (20, 9, 1))
 
-# #Prepare the dataset
-# data = prepare_data(input=input, edge_index=edge_index, labels=labels)
+#Prepare the dataset
+data = prepare_data(input=input, edge_index=edge_index, labels=labels)
 
 
-# class SimpleCustomBatch:
-#     def __init__(self, data):
-#         data = prepare_data(input=input, edge_index=edge_index, labels=labels)
-#         self.inp = data.x
-#         self.tgt = data.y
+class SimpleCustomBatch:
+    def __init__(self, data):
+        data = prepare_data(input=input, edge_index=edge_index, labels=labels)
+        self.inp = data.x
+        self.tgt = data.y
 
-#     # custom memory pinning method on custom type
-#     def pin_memory(self):
-#         self.inp = self.inp.pin_memory()
-#         self.tgt = self.tgt.pin_memory()
-#         return self
+    # custom memory pinning method on custom type
+    def pin_memory(self):
+        self.inp = self.inp.pin_memory()
+        self.tgt = self.tgt.pin_memory()
+        return self
 
-# def collate_wrapper(batch):
-#     return SimpleCustomBatch(batch)
+def collate_wrapper(batch):
+    return SimpleCustomBatch(batch)
 
-# dataset = torch.utils.data.TensorDataset(data.x, data.y)
+dataset = torch.utils.data.TensorDataset(data.x, data.y)
 
-# #Select number of epoch and learning rate
-# epochs = [100, 500, 1000, 10000, 100000]
-# lrs = [0.1, 0.001, 1e-3, 1e-4, 1e-5, 1e-6, 1e-9]
+#Select number of epoch and learning rate
+epochs = [100, 500, 1000, 10000, 100000]
+lrs = [0.1, 0.001, 1e-3, 1e-4, 1e-5, 1e-6, 1e-9]
 
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# #The goal is to demostrate that the model is learning, to do so let's take an array of epochs(epochs) and learning rates (lrs)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#The goal is to demostrate that the model is learning, to do so let's take an array of epochs(epochs) and learning rates (lrs)
 
-# for epoch in epochs:
-#     for lr in lrs:
+for epoch in epochs:
+    for lr in lrs:
         
-#         model = MyGCN().to(device)
-#         model.train(True)
+        model = MyGCN().to(device)
+        model.train(True)
 
-#         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-#         iterations = 0
-#         loss_ar = []
-#         for i in range(epoch):
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+        iterations = 0
+        loss_ar = []
+        for i in range(epoch):
 
-#             testloader = torch.utils.data.DataLoader(dataset, batch_size=4, collate_fn= collate_wrapper, shuffle=True)
+            testloader = torch.utils.data.DataLoader(dataset, batch_size=4, collate_fn= collate_wrapper, shuffle=True)
 
-#             for j, data_in in enumerate(testloader):
-#                 optimizer.zero_grad()
-#                 iterations = iterations + 1
-#                 out = model(x = data_in.inp , edge_index=data.edge_index)
-#                 loss = F.mse_loss(out, data_in.tgt)
-#                 mse_loss = loss.detach().numpy()
-#                 loss_ar.append(mse_loss)
-#                 loss.backward()
-#                 optimizer.step()
+            for j, data_in in enumerate(testloader):
+                optimizer.zero_grad()
+                iterations = iterations + 1
+                out = model(x = data_in.inp , edge_index=data.edge_index)
+                loss = F.mse_loss(out, data_in.tgt)
+                mse_loss = loss.detach().numpy()
+                loss_ar.append(mse_loss)
+                loss.backward()
+                optimizer.step()
 
-#         state_dict = model.state_dict()
-#         torch.save(state_dict, 'weigths/model_weights'+str(epoch)+str(lr)+'.pt')
-#         plot_mse_epoch(iterations, loss_ar, epoch, lr)
+        state_dict = model.state_dict()
+        torch.save(state_dict, 'weigths/model_weights'+str(epoch)+str(lr)+'.pt')
+        plot_mse_epoch(iterations, loss_ar, epoch, lr)
